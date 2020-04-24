ecf87d5c75af90b056c50a1369aed6664ea5b1f2
Charles Knight <32278428+Cyanoth@users.noreply.github.com>
2020-04-24 10:16:24 +0100

    RepositoryDoesNotExistException extend from Exception rather than RuntimeException

--- src/main/java/com/cyanoth/prgroup/exceptions/RepositoryDoesNotExistException.java
+++ src/main/java/com/cyanoth/prgroup/exceptions/RepositoryDoesNotExistException.java
@@ -1,3 +1,3 @@
 package com.cyanoth.prgroup.exceptions;
 
-public class RepositoryDoesNotExistException extends RuntimeException {}
+public class RepositoryDoesNotExistException extends Exception {}
